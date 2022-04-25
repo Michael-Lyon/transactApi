968236d4b952d20944daef36f106127316e7c18c
Michael-Lyon <mickiasomg@gmail.com>
2022-04-25 19:07:36 +0100

    Chnaged database back to sqlite1

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_myapi',
-        'USER': 'api',
-        'PASSWORD': 'roots',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_myapi',
+#         'USER': 'api',
+#         'PASSWORD': 'roots',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
